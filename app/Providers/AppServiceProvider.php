ca6dfd69d81a866a0e49d233456151f580e4c6bd
tanaka <bfmt1250081@gn.iwasaki.ac.jp>
2018-10-28 18:19:14 +0900

    Add Procfile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-    }
+        // 本番環境(Heroku)でhttpsを強制する
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
 
     /**
      * Register any application services.
